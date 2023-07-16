4277918b3818dcaebeee715808b0908ea8c96dde
Khue Doan <mail@khuedoan.com>
2023-07-17 01:07:40 +0700

    feat: enable GPG agent with SSH agent emulation

--- configuration.nix
+++ configuration.nix
@@ -50,7 +50,6 @@
     gcc
     git
     gnumake
-    gnupg
     neovim
     tmux
     tree
@@ -63,10 +62,11 @@
   # started in user sessions.
   # programs.mtr.enable = true;
   programs.zsh.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
 
   # List services that you want to enable:
 
