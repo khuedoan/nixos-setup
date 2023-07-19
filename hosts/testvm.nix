ee9970344df45ca551cf27bc38d6faa75fc74a3c
Khue Doan <mail@khuedoan.com>
2023-07-19 13:41:26 +0700

    fix: enable GPU support for the test VM

--- hosts/testvm.nix
+++ hosts/testvm.nix
@@ -1,5 +1,16 @@
+{ modulesPath, ... }:
+
 {
-   users.users.khuedoan = {
-     password = "testvm";
-   };
+  imports = [
+    (modulesPath + "/virtualisation/qemu-vm.nix")
+  ];
+
+  virtualisation.qemu.options = [
+    "-device virtio-vga-gl"
+    "-display gtk,gl=on"
+  ];
+
+  users.users.khuedoan = {
+    password = "testvm";
+  };
 }
