d6cb682b3e7fdfed2ecc4f0693eb394db56d121f
Khue Doan <mail@khuedoan.com>
2023-11-30 00:21:05 +0700

    chore: update NixOS channel to 23.11

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-23.05";
+      url = "github:nixos/nixpkgs/nixos-23.11";
     };
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
