fe98f8bc9b59a3d3ac71c822ae15ee36303d781b
Khue Doan <mail@khuedoan.com>
2024-06-09 09:18:15 +0700

    chore: upgrade to NixOS 24.05

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-23.11";
+      url = "github:nixos/nixpkgs/nixos-24.05";
     };
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware/master";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
