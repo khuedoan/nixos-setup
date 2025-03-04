a097a4331044b4a5a5e8382278b5d46e151b5e51
Khue Doan <mail@khuedoan.com>
2025-03-04 13:33:15 +0700

    fix: get all card power config

--- scripts/check_laptop_power.sh
+++ scripts/check_laptop_power.sh
@@ -9,7 +9,7 @@ cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor | uniq
 cat /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference | uniq
 
 echo "===== GPU ====="
-cat /sys/class/drm/card0/device/power_dpm_force_performance_level
+cat /sys/class/drm/card*/device/power_dpm_force_performance_level
 
 echo "===== Platform ====="
 cat /sys/firmware/acpi/platform_profile
