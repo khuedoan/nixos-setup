9fae9c561a1f06f10edba206aa25f59e04c5ad19
Khue Doan <mail@khuedoan.com>
2026-04-25 14:33:57 +0700

    test: activate all profiles

--- hosts/macos-test.nix
+++ hosts/macos-test.nix
@@ -1,17 +1,18 @@
-{ config, lib, ... }:
+{ lib, ... }:
 
 {
+  imports = [
+    ../modules/cli
+    ../modules/dotfiles
+    ../modules/gui
+    ../modules/personal
+    ../modules/work
+  ];
+
   primaryUser.username = "runner";
 
   networking.hostName = "macos-test";
 
-  # Linux builder is disabled in CI until bootstrap issues are fixed.
+  # TODO Linux builder is disabled in CI until bootstrap issues are fixed.
   nix.linux-builder.enable = lib.mkForce false;
-
-  home-manager = {
-    users.${config.primaryUser.username} = {
-      home.stateVersion = "25.11";
-      programs.home-manager.enable = true;
-    };
-  };
 }
